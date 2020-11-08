52875a970163541908e575f4c60ad2c4c71ddf8f
athet <illia.nesterenko@nure.ua>
2020-11-08 23:06:13 +0200

    Final

---
+++ script.js
@@ -0,0 +1,7 @@
+﻿let progress = document.getElementById('progress_bar');
+
+window.onscroll = function() {
+    let total_height = document.body.scrollHeight - window.innerHeight;
+    let progress_height = (window.pageYOffset / total_height) * 100;
+    progress.style.height = progress_height + "%";
+}
